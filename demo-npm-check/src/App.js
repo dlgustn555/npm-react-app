99728d6a291ada2a50d6a0e2112aa9c5c38d3e9a
hyunsu-lee <hyunsu.lee@navercorp.com>
2021-11-16 17:41:22 +0900

    init

---
+++ demo-npm-check/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {InputBox} from '@dlgustn555/input-list'
+
+function App() {
+  return (
+    <div className="App">
+      <InputBox />
+    </div>
+  );
+}
+
+export default App;
