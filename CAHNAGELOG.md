fad0abc29e52186ff951ab1bf8d2cff75edca22b
hyunsu-lee <hyunsu.lee@navercorp.com>
2021-11-18 13:18:31 +0900

    init

---
+++ CAHNAGELOG.md
@@ -0,0 +1,4 @@
+# CHANGELOG
+
+## 2.1.6
+- changelog 파일 테스트 
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,2 @@
-export { default as InputBox } from './InputBox';
-export { default as Button } from './Button';
+export { default as InputBox } from "./InputBox";
+export { default as Button } from "./Button";
